0ce99344230958b60557a22a2de6f1c8f844a2fc
osunGodfrey <149335879+osunGodfrey@users.noreply.github.com>
2026-06-17 00:42:21 -0700

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = s.lower()
+        alphanums = set("abcdefghijklmnopqrstuvwxyz0987654321")
+        alphanums_s = ""
+        flip_s = ""
+        for letter in s:
+            if letter in alphanums:
+                alphanums_s = alphanums_s + letter
+                flip_s = letter + flip_s
+        # print(flip_s)
+        return flip_s == alphanums_s
+        
