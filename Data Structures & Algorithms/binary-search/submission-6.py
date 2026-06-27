d3433361d31a06cbebf076e24643249e6c47a8bf
osunGodfrey <149335879+osunGodfrey@users.noreply.github.com>
2026-06-27 16:15:07 -0700

    Add: binary-search - submission-6

---
+++ Data Structures & Algorithms/binary-search/submission-6.py
@@ -0,0 +1,39 @@
+import math
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if nums[0] > target:
+            return -1
+        size = len(nums)
+        step = size
+        max_i = size
+        min_i = 0
+        i = 0
+        count = 0
+        while i < size:
+            step = math.ceil((max_i - min_i) / 2)
+            # print("step", step)
+            # print("min", min_i, "max", max_i)
+            # print("index", i,"value", nums[i], "targe", target)
+            if nums[i] < target:
+                min_i = i + step
+                i = i + step
+                # print("-->", i)
+            elif nums[i] > target:
+                max_i = i
+                min_i = i - step
+                i = i - step
+                # print("<--", i)
+            elif nums[i] == target:
+                # print("==", i)
+                return i
+            elif step <= 0:
+                return -1
+            else:
+                return None
+            # step = (max_i - min_i) // 2
+            # count += 1
+            if step <= 0: return -1
+            # if count == 5: return -4
+            # print("------")
+        return -1
+        
